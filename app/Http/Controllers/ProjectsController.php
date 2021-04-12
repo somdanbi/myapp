e955a25406901cc1052dc4ec520d2961070eb03a
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-04-12 03:52:56 -0500

    Just Refactoring

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -44,7 +44,7 @@ class ProjectsController extends Controller
      */
     public function show(Project $project)
     {
-        if ( auth()->id() != $project->owner_id ) {
+        if ( auth()->user()->isNot($project->owner)) {
             abort(403);
         }
         return view('projects.show', compact('project'));
