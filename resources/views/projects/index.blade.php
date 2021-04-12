291796edc270e08fc7ed6bee206b9b7a4de5e3d6
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-04-12 05:58:51 -0500

    Cleaning code before using Tailwind

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,34 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header"><h1>Birdboad</h1></div>
 
-                    <div class="card-body">
-                        <a href="/projects/create" class="btn btn-secondary">Create</a>
-
-                        <ul>
-                            @forelse($projects as $project)
-
-
-                                <li>
-                                    <a href="{{$project->path()}}">
-                                        {{$project->title}}
-                                    </a>
-                                </li>
-                            @empty
-                                <li>No projects yet</li>
-                            @endforelse
-
-                        </ul>
-
-
-                    </div>
-                </div>
-            </div>
+        <div class="">
+            <h1>Birdboad</h1>
+            <a href="/projects/create" class="btn btn-secondary">Create</a>
         </div>
-    </div>
+
+
+
+
+            <ul>
+                @forelse($projects as $project)
+
+
+                    <li>
+                        <a href="{{$project->path()}}">
+                            {{$project->title}}
+                        </a>
+                    </li>
+                @empty
+                    <li>No projects yet</li>
+                @endforelse
+
+            </ul>
+
+
+
+
+
 @endsection
